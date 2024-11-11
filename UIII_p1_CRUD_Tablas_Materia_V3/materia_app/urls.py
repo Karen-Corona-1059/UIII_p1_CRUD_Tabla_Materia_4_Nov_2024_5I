5c0a9022f8edbd037a77034d34432ec4bf6e8b6b
Karen Corona <164534032+Karen-Corona-1059@users.noreply.github.com>
2024-11-11 14:04:02 -0600

    CRUD terminado

---
+++ UIII_p1_CRUD_Tablas_Materia_V3/materia_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from materia_app import views
+
+
+urlpatterns = [
+    path("", views.inicio_vista, name="inicio_vista"),
+    path("registraMateria/",views.registrarMateria,name="registrarMateria"),
+
+    path("borrarMateria/<codigo>",views.borrarMateria,name="borrarMateria"),
+    path("seleccionarMateria/<codigo>",views.seleccionarMateria,name="seleccionarMateria"),
+    path("editarMateria/",views.editarMateria,name="editarMateria")
+]
